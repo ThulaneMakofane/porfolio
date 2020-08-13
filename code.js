10eaeda493f4bb3c04a024a690c77f0c22aa0d7c
Scottyguytumblr <62024866+Scottyguytumblr@users.noreply.github.com>
2020-08-13 18:01:27 +0200

    Add files via upload

---
+++ code.js
@@ -0,0 +1,35 @@
+function onClickMenu() {
+  document.getElementById("menu").classList.toggle("change");
+  document.getElementById("nav").classList.toggle("change");
+
+  document.getElementById("menu-bg").classList.toggle("change-bg");
+}
+
+$(document).ready(function () {
+  $(".submit").click(function (event) {
+    var email = $(".Email").val();
+    var subject = $(".Subject").val();
+    var services = $(".Services").val();
+    var name = $(".Name").val();
+    var statusElm = $(".status");
+    statusElm.empty();
+
+    if (email.lenght >= 5 && email.includes("@") && email.includes(".")) {
+      statusElm.append("<div>valid  email</div>");
+    } else {
+      statusElm.append(" <div> invalid email</div >");
+    }
+
+    if (name.lenght >= 2) {
+      statusElm.append("<div> valid name</div >");
+    } else {
+      statusElm.append("<div>your name is invalid</div >");
+    }
+
+    if (subject.lenght >= 10) {
+      statusElm.append("<div> valid message</div >");
+    } else {
+      statusElm.append("<div> please add more on your message </div >.");
+    }
+  });
+});
